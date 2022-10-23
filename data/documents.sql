f8fede426742b90efe4deccee3246a7a8e714398
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-10-23 08:58:10 +0530

    Update documents.sql

--- data/documents.sql
+++ data/documents.sql
@@ -150,56 +150,52 @@ CREATE INDEX idx_page_document ON pages ( document_id );-- --
 CREATE INDEX idx_page_sort ON pages ( sort_order );-- --
 
 -- Content breakpoints
-CREATE TABLE page_lines (
+CREATE TABLE page_blocks (
 	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	content TEXT NOT NULL DEFAULT '{ "body" : "" }' COLLATE NOCASE, 
+	
+	body TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.body' ), "" )
+	) STORED NOT NULL, 
 	page_id INTEGER NOT NULL,
 	sort_order INTEGER NOT NULL DEFAULT 0,
 	
-	CONSTRAINT fk_line_page 
+	CONSTRAINT fk_block_page 
 		FOREIGN KEY ( page_id ) 
 		REFERENCES pages ( id )
 		ON DELETE CASCADE
 );-- --
-CREATE INDEX idx_line_page ON page_lines ( page_id );-- --
-CREATE INDEX idx_line_sort ON page_lines ( sort_order );-- --
+CREATE INDEX idx_block_page ON page_blocks ( page_id );-- --
+CREATE INDEX idx_block_sort ON page_blocks ( sort_order );-- --
 
 -- Content searching
-CREATE VIRTUAL TABLE line_search 
+CREATE VIRTUAL TABLE block_search 
 	USING fts4( body, tokenize=unicode61 );-- --
 
-CREATE TRIGGER line_insert AFTER INSERT ON page_lines FOR EACH ROW 
-WHEN NEW.content IS NOT ""
+CREATE TRIGGER block_insert AFTER INSERT ON page_blocks FOR EACH ROW
+WHEN NEW.body IS NOT ""
 BEGIN
 	-- Create search data
-	INSERT INTO line_search( docid, body ) 
-		VALUES ( 
-			NEW.id, 
-			json_extract( NEW.content, '$.body' )
-		);
-	
+	INSERT INTO block_search( docid, body ) 
+		VALUES ( NEW.id, NEW.body );
 END;-- --
 
-CREATE TRIGGER line_update AFTER INSERT ON page_lines FOR EACH ROW 
-WHEN NEW.content IS NOT ""
+CREATE TRIGGER block_update AFTER UPDATE ON page_blocks FOR EACH ROW
+WHEN NEW.body IS NOT ""
 BEGIN
-	REPLACE INTO line_search( docid, body ) 
-		VALUES ( 
-			NEW.id, 
-			json_extract( NEW.content, '$.body' )
-		);
-	
+	REPLACE INTO block_search( docid, body ) 
+		VALUES ( NEW.id, NEW.body );
 END;-- --
 
-CREATE TRIGGER line_clear AFTER UPDATE ON page_lines FOR EACH ROW 
-WHEN NEW.content IS ""
+CREATE TRIGGER block_clear AFTER UPDATE ON page_blocks FOR EACH ROW 
+WHEN NEW.body IS ""
 BEGIN
-	DELETE FROM line_search WHERE docid = NEW.id;
+	DELETE FROM block_search WHERE docid = NEW.id;
 END;-- --
 
-CREATE TRIGGER line_delete BEFORE DELETE ON page_lines FOR EACH ROW 
+CREATE TRIGGER block_delete BEFORE DELETE ON page_blocks FOR EACH ROW 
 BEGIN
-	DELETE FROM memo_search WHERE docid = OLD.id;
+	DELETE FROM block_search WHERE docid = OLD.id;
 END;-- --
 
 
@@ -208,6 +204,9 @@ CREATE TABLE memos (
 	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	content TEXT NOT NULL DEFAULT '{ "body" : "" }' COLLATE NOCASE, 
 	
+	body TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.body' ), "" )
+	) STORED NOT NULL,
 	created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );-- --
@@ -223,10 +222,7 @@ WHEN NEW.content IS NOT ""
 BEGIN
 	-- Create search data
 	INSERT INTO memo_search( docid, body ) 
-		VALUES ( 
-			NEW.id, 
-			json_extract( NEW.content, '$.body' )
-		);
+		VALUES ( NEW.id, NEW.body );
 	
 END;-- --
 
@@ -234,10 +230,7 @@ CREATE TRIGGER memo_update AFTER UPDATE ON memos FOR EACH ROW
 WHEN NEW.content IS NOT ""
 BEGIN
 	REPLACE INTO memo_search( docid, body ) 
-		VALUES ( 
-			NEW.id, 
-			json_extract( NEW.content, '$.body' )
-		);
+		VALUES ( NEW.id, NEW.body );
 	
 	UPDATE memos SET updated = CURRENT_TIMESTAMP 
 		WHERE id = NEW.id;
@@ -256,38 +249,39 @@ BEGIN
 END;-- --
 
 
-CREATE TABLE memo_lines (
+CREATE TABLE memo_blocks (
 	memo_id INTEGER NOT NULL,
-	line_id INTEGER NOT NULL,
+	block_id INTEGER NOT NULL,
 	begin_range INTEGER NOT NULL DEFAULT 0,
 	end_range INTEGER NOT NULL DEFAULT 0,
 	
-	CONSTRAINT fk_line_memo  
+	CONSTRAINT fk_block_memo  
 		FOREIGN KEY ( memo_id ) 
 		REFERENCES memos ( id )
 		ON DELETE CASCADE,
 	
-	CONSTRAINT fk_memo_line
-		FOREIGN KEY ( line_id ) 
-		REFERENCES lines ( id )
+	CONSTRAINT fk_memo_block
+		FOREIGN KEY ( block_id ) 
+		REFERENCES page_blocks ( id )
 		ON DELETE CASCADE
 );-- --
-CREATE INDEX idx_memo_line ON memo_lines ( memo_id, line_id );-- --
-CREATE UNIQUE INDEX idx_memo_line_range ON memo_lines 
-	( memo_id, line_id, begin_range, end_range )
+CREATE INDEX idx_memo_block ON memo_blocks ( memo_id, block_id );-- --
+CREATE UNIQUE INDEX idx_memo_block_range ON memo_blocks 
+	( memo_id, block_id, begin_range, end_range )
 	WHERE begin_range IS NOT 0 AND end_range IS NOT 0;-- --
 
 
 -- Remove orphaned memo associations
-CREATE TRIGGER memo_line_update AFTER UPDATE ON memo_lines FOR EACH ROW
+CREATE TRIGGER memo_block_update AFTER UPDATE ON memo_blocks FOR EACH ROW
 BEGIN
-	DELETE FROM memo_lines WHERE begin_range = end_range;
+	DELETE FROM memo_blocks WHERE begin_range = end_range;
 END;-- --
 
 -- Authorship
 CREATE TABLE user_documents (
 	user_id, INTEGER NOT NULL,
 	document_id INTEGER NOT NULL,
+	status INTEGER NOT NULL DEFAULT 0,
 	
 	CONSTRAINT fk_document_user 
 		FOREIGN KEY ( user_id ) 
@@ -299,12 +293,14 @@ CREATE TABLE user_documents (
 		REFERENCES documents ( id )
 		ON DELETE CASCADE
 );-- --
-CREATE UNIQUE INDEX idx_user_document 
+CREATE UNIQUE INDEX idx_user_doc
 	ON user_documents ( user_id, document_id );-- --
+CREATE INDEX idx_user_doc_status ON user_documents ( status );-- --
 
 CREATE TABLE user_pages (
 	user_id, INTEGER NOT NULL,
 	page_id INTEGER NOT NULL,
+	status INTEGER NOT NULL DEFAULT 0,
 	
 	CONSTRAINT fk_page_user 
 		FOREIGN KEY ( user_id ) 
@@ -317,10 +313,30 @@ CREATE TABLE user_pages (
 		ON DELETE CASCADE
 );-- --
 CREATE UNIQUE INDEX idx_user_page ON user_pages ( user_id, page_id );-- --
+CREATE INDEX idx_user_page_status ON user_pages ( status );-- --
+
+CREATE TABLE user_blocks (
+	user_id, INTEGER NOT NULL,
+	block_id INTEGER NOT NULL,
+	status INTEGER NOT NULL DEFAULT 0,
+	
+	CONSTRAINT fk_page_user 
+		FOREIGN KEY ( user_id ) 
+		REFERENCES users ( id )
+		ON DELETE CASCADE,
+	
+	CONSTRAINT fk_user_block 
+		FOREIGN KEY ( block_id ) 
+		REFERENCES page_blocks ( id )
+		ON DELETE CASCADE
+);-- --
+CREATE UNIQUE INDEX idx_user_block ON user_blocks ( user_id, block_id );-- --
+CREATE INDEX idx_user_block_status ON user_blocks ( status );-- --
 
 CREATE TABLE user_memos (
 	user_id, INTEGER NOT NULL,
 	memo_id INTEGER NOT NULL,
+	status INTEGER NOT NULL DEFAULT 0,
 	
 	CONSTRAINT fk_memo_user 
 		FOREIGN KEY ( user_id ) 
