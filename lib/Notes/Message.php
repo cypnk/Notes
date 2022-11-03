a56c097a65bd95fc457b3019170a25292ce200fd
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-11-03 14:33:27 +0530

    Create Message.php

---
+++ lib/Notes/Message.php
@@ -0,0 +1,84 @@
+<?php declare( strict_types = 1 );
+
+namespace Notes;
+
+class Message {
+	
+	/**
+	 *  Content MIME type
+	 *  @var string
+	 */
+	public readonly $content_type;
+	
+	/**
+	 *  Message headers list
+	 *  @var array
+	 */
+	protected readonly headers $headers;
+	
+	/**
+	 *  Message headers list
+	 *  @var array
+	 */
+	public readonly array $lv_headers;
+	
+	/**
+	 *  Message source or destination URI
+	 *  @var string
+	 */
+	protected readonly string $uri;
+	
+	/**
+	 *  Message protocol. E.G. HTTP 1.1
+	 *  @var string
+	 */
+	protected readonly string $protocol;
+	
+	/**
+	 *  Current message querystring or path attachment
+	 *  @var string
+	 */
+	protected readonly string $querystring;
+	
+	/**
+	 *  Core settings and configuration 
+	 *  @var \PubCabin\Config
+	 */
+	protected readonly object $config;
+	
+	/**
+	 *  Main event controller
+	 *  @var \PubCabin\Controller
+	 */
+	protected readonly object $ctrl;
+	
+	public function __construct( Controller $ctrl ) {
+		$this->ctrl	= $ctrl;
+		$this->config	= $ctrl->getConfig();
+	}
+	
+	/**
+	 *  Get or guess current server protocol
+	 *  
+	 *  @param string	$assume		Default protocol to assume if not given
+	 *  @return string
+	 */
+	public function getProtocol( string $assume = 'HTTP/1.1' ) : string {
+		if ( isset( $this->protocol ) ) {
+			return $this->protocol;
+		}
+		$this->protocol = $_SERVER['SERVER_PROTOCOL'] ?? $assume;
+		return $this->protocol;
+	}
+	
+	/**
+	 *  Helper to append or replace currently set headers
+	 *  
+	 *  @param array	$_headers	New set of headers
+	 */
+	public function mergeHeaders( array $_headers ) {
+		$this->headers = \array_merge( $this->headers, $_headers );
+	}
+}
+
+
