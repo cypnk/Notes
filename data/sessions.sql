25185887636321598619d526238c147faeaf1fa9
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-11-14 20:43:24 +0530

    Create sessions.sql

---
+++ data/sessions.sql
@@ -0,0 +1,18 @@
+
+-- Visitor/User sessions
+CREATE TABLE sessions(
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	session_id TEXT DEFAULT NULL COLLATE NOCASE,
+	session_data TEXT DEFAULT NULL COLLATE NOCASE,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);-- --
+CREATE UNIQUE INDEX idx_session_id ON sessions( session_id );-- --
+CREATE INDEX idx_session_created ON sessions( created DESC );-- --
+CREATE INDEX idx_session_updated ON sessions( updated DESC );-- --
+
+CREATE TRIGGER session_update AFTER UPDATE ON sessions
+BEGIN
+	UPDATE sessions SET updated = CURRENT_TIMESTAMP 
+		WHERE id = NEW.id;
+END;
