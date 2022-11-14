4660fa5f7796acf571ca3000ebd3b1dbed069c44
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-11-14 17:29:00 +0530

    Update Notes.h

--- app/src/headers/Notes.h
+++ app/src/headers/Notes.h
@@ -4,10 +4,20 @@
 // On Visual Studio
 //#define _CRT_SECURE_NO_WARNINGS
 
+//#include <fstream>
+//#include <sstream>
+//#include <SDL2/SDL.h>
+#include "NotesCommands.h"
+#include "NotesFormatting.h"
+//#include "NotesData.h"
+#include "NotesInput.h"
 
-// Cursor and cursor clear symbols
-#define	CUR_SYMBOL		"▋"
-#define CLR_SYMBOL		" "
+/**
+ *  Main event loop delay
+ */
+#define LOOP_WAIT	30
+
 
 
 #endif
+
