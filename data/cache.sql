2fc371c967af4bda8092a0f09b2968ce6244d7e6
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-11-14 20:44:54 +0530

    Create cache.sql

---
+++ data/cache.sql
@@ -0,0 +1,54 @@
+
+-- Cache tables
+CREATE TABLE caches (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+	cache_id TEXT NOT NULL COLLATE NOCASE, 
+	ttl INTEGER NOT NULL, 
+	content TEXT NOT NULL COLLATE NOCASE, 
+	expires DATETIME DEFAULT NULL,
+	created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+	updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);-- --
+
+CREATE UNIQUE INDEX idx_caches_on_cache_id ON caches ( cache_id ASC );-- --
+CREATE INDEX idx_caches_on_expires ON caches ( expires DESC )
+	WHERE expires IS NOT NULL;-- --
+CREATE INDEX idx_caches_on_created ON caches ( created ASC );-- --
+CREATE INDEX idx_caches_on_updated ON caches ( updated );-- --
+
+-- Cache triggers
+CREATE TRIGGER cache_after_insert AFTER INSERT ON caches FOR EACH ROW 
+BEGIN
+	-- Generate expiration
+	UPDATE caches SET 
+		expires = datetime( 
+			( strftime( '%s','now' ) + NEW.ttl ), 
+			'unixepoch' 
+		) WHERE rowid = NEW.rowid;
+	
+	-- Clear expired data
+	DELETE FROM caches WHERE 
+		strftime( '%s', expires ) < 
+		strftime( '%s', updated ) AND expires IS NOT NULL;
+END;-- --
+
+-- Change only update period when TTL is empty
+CREATE TRIGGER cache_after_update AFTER UPDATE ON caches FOR EACH ROW 
+WHEN NEW.updated < OLD.updated AND NEW.ttl = 0
+BEGIN
+	UPDATE caches SET updated = CURRENT_TIMESTAMP 
+		WHERE rowid = NEW.rowid;
+END;-- --
+
+-- Change expiration period when TTL exists
+CREATE TRIGGER cache_after_update_ttl AFTER UPDATE ON caches FOR EACH ROW 
+WHEN NEW.updated < OLD.updated AND NEW.ttl <> 0
+BEGIN
+	-- Change expiration
+	UPDATE caches SET updated = CURRENT_TIMESTAMP, 
+		expires = datetime( 
+			( strftime( '%s','now' ) + NEW.ttl ), 
+			'unixepoch' 
+		) WHERE rowid = NEW.rowid;
+END;
+
