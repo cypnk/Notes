cfcef46f93c10efcb84857f5dd9a0c57e296d6b4
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-11-14 15:01:37 +0530

    Create Notes.h

---
+++ app/src/headers/Notes.h
@@ -0,0 +1,13 @@
+#ifndef NOTES_H
+#define NOTES_H
+
+// On Visual Studio
+//#define _CRT_SECURE_NO_WARNINGS
+
+
+// Cursor and cursor clear symbols
+#define	CUR_SYMBOL		"▋"
+#define CLR_SYMBOL		" "
+
+
+#endif
