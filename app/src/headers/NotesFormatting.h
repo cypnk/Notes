166f9e0edf8e8ac39e84cb144e1d7dd345372204
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-11-14 17:30:15 +0530

    Create NotesFormatting.h

---
+++ app/src/headers/NotesFormatting.h
@@ -0,0 +1,39 @@
+
+
+/**
+ *  Default font settings
+ */
+#define FONT_FILE	"bin/assets/fonts/NotoSansMono-Regular.ttf"
+#define FONT_SIZE	13
+
+
+/**
+ *  Default text direction
+ */
+#define TEXT_LTR	0x0000	// Left to right
+#define TEXT_RTL	0x0001	// Right to left
+#define TEXT_TDL	0x0002	// Top to bottom, left to right
+#define TEXT_TDR	0x0003	// Top to bottom, right to left
+
+
+/**
+ *  Page turn direction
+ */
+#define PAGE_TD		0x0000	// Top to down
+#define PAGE_LR		0x0001	// Left to right
+#define PAGE_RL		0x0002	// Right to left
+
+
+/**
+ *  Formatting type
+ */
+#define	F_NORMAL	0x0000
+#define	F_BOLD		0x0001
+#define	F_ITALIC	0x0002
+#define	F_UNDER		0x0003
+#define	F_SUP		0x0004
+#define	F_SUB		0x0005
+
+
+#endif
+
