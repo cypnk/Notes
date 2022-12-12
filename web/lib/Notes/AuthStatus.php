5a65332c1d643f3409ad4fb96a7b609544abd509
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-12-11 21:07:34 -0500

    Create AuthStatus.php

---
+++ web/lib/Notes/AuthStatus.php
@@ -0,0 +1,58 @@
+<?php declare( strict_types = 1 );
+
+namespace Notes;
+
+enum AuthStatus {
+	case Success;
+	case Failed;
+	
+	case NoUser;
+	case Duplicate;
+	case LockedOut;
+	case Unapproved;
+	
+	case PinError;
+	case EmailError;
+	
+	case AuthProviderError;
+	case PermProviderError;
+	
+	case UserError;
+	case SessionError;
+	case CookieError;
+	case RealmError;
+	case ScopeError;
+	case RoleError;
+	
+	/**
+	 *  Success or failure mode groups
+	 *  @return string
+	 */
+	public function mode() : string {
+		return 
+		match( $this ) {
+			AuthStatus::Success,
+			AuthStatus::Failed,
+			AuthStatus::Duplicate,
+			AuthStatus::NoUser		=> 'credentials',
+			
+			AuthStatus::PinError,
+			AuthStatus::EmailError		=> 'twofactor',
+			
+			AuthStatus::LockedOut,
+			AuthStatus::Unapproved,
+			AuthStatus::RoleError		=> 'permissions',
+			
+			AuthStatus::AuthProviderError,
+			AuthStatus::PermProviderError	=> 'provider',
+			
+			AuthStatus::UserError,
+			AuthStatus::SessionError,
+			AuthStatus::CookieError		=> 'session',
+			
+			AuthStatus::RealmError,
+			AuthStatus::ScopeError		=> 'location'
+		}
+	}
+}
+
