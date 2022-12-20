041af433a20612717c5a0d5d8b9948b9f97357e5
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-12-20 12:20:41 -0500

    Create CacheHandler.php

---
+++ web/lib/Notes/CacheHandler.php
@@ -0,0 +1,113 @@
+<?php declare( strict_types = 1 );
+
+namespace Notes;
+
+class CacheHandler extends Controllable {
+	
+	/**
+	 *  Default cache timeout
+	 */
+	const CACHE_TTL	= 7200;
+	
+	/**
+	 *  Currently loaded configuration updated date
+	 *  @var string
+	 */
+	private readonly string $cache_updated;
+	
+	/**
+	 *  Get specific cache by identifier
+	 *  
+	 *  @return mixed
+	 */
+	public function getCache( string $key ) {
+		$res = $this->getCacheSet( [ $key ] );
+		
+		return empty( $res ) ? null : $res[0];
+	}
+	
+	/**
+	 *  Get collection of saved caches by key collection
+	 *  
+	 *  @param array	$keys	List of unique identifiers
+	 *  @return
+	 */
+	public function getCacheSet( array $keys ) : array {
+		// Format to current config
+		$keys	= \array_map( [ $this, 'genCacheKey' ], $keys );
+		
+		// Nothing to find
+		if ( empty( $keys ) ) {
+			return [];
+		}
+		
+		$db	= $this->controller->getData();
+		$params	= [];
+		$in	= $db->getInParam( $keys, $params );
+		
+		return 
+		$db->getResults( 
+			"SELECT * FROM cache_view WHERE cache_id {$in};", 
+			$params, 
+			\CACHE,
+			'controllable|\\Notes\\Cache'
+		);
+	}
+	
+	/**
+	 *  Save key-value set of cache data and optional TTL
+	 *  
+	 *  @param array	$data	Cache info in [id,content,TTL] format
+	 *  @return array
+	 */
+	public function saveCacheData( array $data ) : array {
+		$k = [];
+		foreach ( $data as $c ) {
+			$i		= new \Notes\Cache();
+			$i->cache_id	= $this->genCacheKey( $c[0] );
+			$i->content	= $c[1];
+			$i->ttl		= 
+				( int ) ( $c[2] ?? static::CACHE_TTL );
+			
+			$i->save();
+			$k[]		= $i;
+		}
+		
+		return $k;
+	}
+	
+	/**
+	 *  Save given set of cache objects and return id with save status
+	 *  
+	 *  @param array	$caches		\Notes\Cache objects
+	 *  @return array
+	 */
+	public function saveCacheSet( array $caches ) : array {
+		$a = [];
+		foreach ( $caches as $c ) {
+			$a[] = [$c->cache_id, $c->save()];
+		}
+		
+		return $a;
+	}
+	
+	/**
+	 *  Generate cache key for the given identifier
+	 *  This function lets caches be invalidated if config has been modified
+	 *  
+	 *  @param string	$key	Original, identifier as cache key
+	 *  @return string
+	 */
+	public function genCacheKey( string $key ) {
+		if ( !isset( $this->cache_updated ) ) {
+			$this->cache_updated	= 
+			$this->controller->getConfig()->updated ?? '';
+		}
+		
+		return 
+		\hash( 'sha256', $this->cache_updated . trim( $key ) );
+	}
+	
+}
+
+
