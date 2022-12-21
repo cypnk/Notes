a53b9ab30ac4166fdd7286380c7b54937d102e42
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-12-21 18:07:57 -0500

    Update logs.sql

--- data/logs.sql
+++ data/logs.sql
@@ -9,10 +9,56 @@ CREATE TABLE event_logs (
 	body TEXT GENERATED ALWAYS AS ( 
 		COALESCE( json_extract( content, '$.body' ), "" )
 	) STORED NOT NULL,
+	host TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.host' ), NULL )
+	) STORED,
+	secure TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.secure' ), NULL )
+	) STORED,
+	ip TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.ip' ), NULL )
+	) STORED,
+	method TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.method' ), NULL )
+	) STORED,
+	uri TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.uri' ), NULL )
+	) STORED,
+	user_agent TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.user_agent' ), NULL )
+	) STORED,
+	query_string TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.query_string' ), NULL )
+	) STORED,
+	language TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.language' ), NULL )
+	) STORED,
+	file_range TEXT GENERATED ALWAYS AS ( 
+		COALESCE( json_extract( content, '$.file_range' ), NULL )
+	) STORED,
+	
 	created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	status INTEGER DEFAULT 0
 }
 CREATE INDEX idx_log_label ON event_logs ( label );-- --
+CREATE INDEX idx_log_host ON event_logs ( host )
+	WHERE host IS NOT NULL;-- --
+CREATE INDEX idx_log_secure ON event_logs ( secure )
+	WHERE secure IS NOT NULL;-- --
+CREATE INDEX idx_log_ip ON event_logs ( ip )
+	WHERE ip IS NOT NULL;-- --
+CREATE INDEX idx_log_method ON event_logs ( method )
+	WHERE method IS NOT NULL;-- --
+CREATE INDEX idx_log_uri ON event_logs ( uri )
+	WHERE uri IS NOT NULL;-- --
+CREATE INDEX idx_log_ua ON event_logs ( user_agent )
+	WHERE user_agent IS NOT NULL;-- --
+CREATE INDEX idx_log_qs ON event_logs ( query_string )
+	WHERE query_string IS NOT NULL;-- --
+CREATE INDEX idx_log_lang ON event_logs ( language )
+	WHERE language IS NOT NULL;-- --
+CREATE INDEX idx_log_fr ON event_logs ( file_range )
+	WHERE file_range IS NOT NULL;-- --
 CREATE INDEX idx_log_created ON event_logs ( created );-- --
 CREATE INDEX idx_log_status ON event_logs ( status );-- --
 
