e5f5b760884799f11dbc357ed951d489a6ff12db
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-02 17:32:46 -0500

    Update FormLoginProvider.php

--- web/lib/Notes/FormLoginProvider.php
+++ web/lib/Notes/FormLoginProvider.php
@@ -11,39 +11,6 @@ class FormLoginProvider extends IDProvider {
 	}
 	
 	/**
-	 *  Reset authenticated user data types for processing
-	 *  
-	 *  @param array	$user	Stored user in database/session
-	 *  @return array
-	 */
-	public function formatAuthUser( array $user ) : array {
-		$user['is_approved']	??= false;
-		$user['is_locked']	??= false;
-		$user['user_settings']	??= [];
-		
-		return [
-			'id'		=> ( int ) ( $user['id'] ?? 0 ), 
-			'status'	=> ( int ) ( $user['status'] ?? 0 ), 
-			'name'		=> $user['name'] ?? '', 
-			'hash'		=> $user['hash'] ?? '',
-			'is_approved'	=> $user['is_approved'] ? true : false,
-			'is_locked'	=> $user['is_locked'] ? true : false, 
-			'auth'		=> $user['auth'] ?? '',
-			'settings'	=> 
-			\is_array( $user['user_settings'] ) ? 
-				$user['user_settings'] : []
-		];
-	}
-	
-	public function logout() {
-		$sess	= $this->getControllerParam( '\\Notes\\SHandler' );
-		$sess->deleteCookie( 'auth' );
-		$sess->sessionCheck( true );
-		
-		$this->auth->resetLookup();
-	}
-	
-	/**
 	 *  Login user by credentials
 	 *  
 	 *  @param string		$username	Login name to search
@@ -66,21 +33,18 @@ class FormLoginProvider extends IDProvider {
 		}
 		
 		// Verify credentials
-		if ( \Notes\User::verifyPassword( $password, $auth->password ) ) {
-			
-			// Refresh password if needed
-			if ( \Notes\User::passNeedsRehash( $auth->password ) ) {
-				\Notes\User::savePassword( $auth->user_id, $password );
-			}
-			
-			$status = AuthStatus::Success;
-			$auth->updateUserActivity( 'login' );
-			
-			return $this->initUserAuth( $auth );
+		if ( !\Notes\User::verifyPassword( $password, $auth->password ) ) {
+			return static::sendFailed( $status );
 		}
 		
-		// Login failiure
-		return static::sendFailed( $status );
+		
+		$status = AuthStatus::Success;
+		$user	= $this->initUserAuth( $auth );
+		
+		// Refresh password if needed
+		$this->refreshPassword( $data[1] );
+		$this->auth->updateUserActivity( 'login' );
+		return $user;
 	}
 	
 }
