596d630020e8dc738df935e0586ac4e67c473b85
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-03 13:33:09 -0500

    Update IDProvider.php

--- web/lib/Notes/IDProvider.php
+++ web/lib/Notes/IDProvider.php
@@ -6,6 +6,8 @@ class IDProvider extends Provider {
 	
 	protected readonly \Notes\UserAuth $_auth;
 	
+	protected readonly \Notes\AuthType $_auth_type;
+	
 	protected static $insert_sql		= 
 	"INSERT INTO id_providers ( sort_order, settings, label )
 		VALUES( :so, :settings, :label );";
@@ -23,24 +25,37 @@ class IDProvider extends Provider {
 	];
 	
 	public function __set( $name, $value ) {
-		switch( $name ) {
-			case 'auth':
-				$this->setAuth( $value );
-				break;
-		}
-		
-		parent::__set( $name, $value );
-	}
-	
-	protected function setAuth( $auth ) {
 		// Intercept any setting warnings/notices
 		\set_error_handler( function( $eno, $emsg, $efile, $eline ) {
 			logException( new \ErrorException( $emsg, 0, $eno, $efile, $eline ) );
 		}, \E_WARNING | \E_USER_NOTICE | \E_USER_WARNING );
 		
+		switch( $name ) {
+			case 'auth':
+				$this->setAuth( $value );
+				break;
+			
+			case 'auth_type':
+				$this->setAuthType( $value );
+				break;
+		}
+		
+		\restore_error_handler();
+		parent::__set( $name, $value );
+	}
+	
+	public function __get( $name ) {
+		switch ( $name ) {
+			case 'auth_type':
+				return 
+				$this->_auth_type ?? \Notes\AuthType::Unkown;
+		}
+		return parent::__get( $name );	
+	}
+	
+	protected function setAuth( $auth ) {
 		if ( isset( $this->_auth ) ) {
 			\trigger_error( 'Attempt to override previously set auth', \E_USER_WARNING );
-			\restore_error_handler();
 			return;
 		}
 		
@@ -50,7 +65,18 @@ class IDProvider extends Provider {
 		}
 		
 		\trigger_error( 'Attempt to set auth not of type Notes\\UserAuth', \E_USER_WARNING );
-		\restore_error_handler();
+	}
+	
+	protected function setAuthType( $value ) {
+		if ( isset( $this->_auth_type ) ) {
+			\trigger_error( 'Attempt to override previously set authentication type', \E_USER_WARNING );
+			return;
+		}
+		if ( $value instanceof \Notes\AuthType ) {
+			$this->_auth_type = $value;
+			return;
+		}
+		\trigger_error( 'Attempt to set authentication not of type Notes\\AuthType', \E_USER_WARNING );
 	}
 	
 	/**
@@ -75,6 +101,15 @@ class IDProvider extends Provider {
 		$this->auth	 	= $auth;
 		
 		return $user;
+	}
+	
+	/**
+	 *  Get current user authentication, if set
+	 *  
+	 *  @return mixed
+	 */
+	public function getAuth() {
+		return $this->_auth ?? null;
 	}
 	
 	/**
@@ -126,17 +161,47 @@ class IDProvider extends Provider {
 	
 	/**
 	 *  Set login failure status and return null
+	 *  
+	 *  @param \Notes\AuthStatus	$status 	Current authentication status
+	 *  @param \Notes\Provider	$provider	Authentication provider
+	 *  @param array		$data		Raw data sent by the user
 	 */
-	public static function sendFailed( \Notes\AuthStatus &$status ) {
+	public static function sendFailed( 
+		\Notes\AuthStatus	&$status, 
+		\Notes\Provider		$provider,
+		array			$data		= []
+	) {
 		$status = \Notes\AuthStatus::Failed;
+		$provider->getController()->run( 
+			'user_login_failed', [ 
+				'provider'	=> $provider, 
+				'data'		=> $data,
+				'status'	=> $status
+			]
+		);
 		return null;
 	}
 	
 	/**
 	 *  Set no user found status and return null
+	 *  
+	 *  @param \Notes\AuthStatus	$status 	Current authentication status
+	 *  @param \Notes\Provider	$provider	Authentication provider
+	 *  @param array		$data		Raw data sent by the user
 	 */
-	public static function sendNoUser( \Notes\AuthStatus &$status ) {
+	public static function sendNoUser( 	
+		\Notes\AuthStatus	&$status,
+		\Notes\Provider		$provider,
+		array			$data		= []
+	) {
 		$status = AuthStatus::NoUser;
+		$provider->getController()->run( 
+			'user_login_nouser', [ 
+				'provider'	=> $provider, 
+				'data'		=> $data,
+				'status'	=> $status
+			]
+		);
 		return null;
 	}
 	
@@ -152,6 +217,9 @@ class IDProvider extends Provider {
 		
 		if ( \Notes\User::passNeedsRehash( $this->_auth->password ) ) {
 			\Notes\User::savePassword( $this->_auth->user_id, $password );
+			$this->controller->run( 
+				'user_password_rehashed', [ 'provider'	=> $this ]
+			);
 		}
 	}
 	
@@ -190,28 +258,68 @@ class IDProvider extends Provider {
 		// Everything else
 		match( $status ) {
 			// Set activity
-			\Notes\AuthStatus::Active	=> 
-				$this->_auth->updateUserActivity( 'active' ),
+			\Notes\AuthStatus::Active	=> ( function() {
+				$this->_auth->updateUserActivity( 'active' );
+				$this->controller->run( 
+					'user_active', [ 
+						'provider'	=> $this, 
+						'user'		=> $user,
+						'status'	=> $status
+					]
+				);
+			} )(),
 			
 			// Login success
-			\Notes\AuthStatus::Success	=> 
-				$this->_auth->updateUserActivity( 'login' ),
+			\Notes\AuthStatus::Success	=> ( function() {
+				$this->_auth->updateUserActivity( 'login' );
+				$this->controller->run( 
+					'user_login', [ 
+						'provider'	=> $this, 
+						'user'		=> $user,
+						'status'	=> $status
+					]
+				);
+			} )(),
 			
 			// Active login failed
 			\Notes\AuthStatus::Failed, 
 			\Notes\AuthStatus::EmailError,
-			\Notes\AuthStatus::PinError	=> 
-				$this->_auth->updateUserActivity( 'failedlogin' ),
+			\Notes\AuthStatus::PinError	=> ( function() {
+				$this->_auth->updateUserActivity( 'failedlogin' );
+				$this->controller->run( 
+					'user_login_failed', [ 
+						'provider'	=> $this, 
+						'user'		=> $user,
+						'status'	=> $status
+					]
+				);
+			} )(),
 			
 			// Change approval status
-			\Notes\AuthStatus::Unapproved	=> 
-				$this->_auth->updateUserActivity( 'unapprove' ),
+			\Notes\AuthStatus::Unapproved	=> ( function() {
+				$this->_auth->updateUserActivity( 'unapprove' );
+				$this->controller->run( 
+					'user_unapproved', [ 
+						'provider'	=> $this, 
+						'user'		=> $user,
+						'status'	=> $status
+					]
+				);
+			} )(),
 			
 			// Lock on provider errors
 			\Notes\AuthStatus::LockedOut,
 			\Notes\AuthStatus::AuthProviderError,
-			\Notes\AuthStatus::PermProviderError	=>
-				$this->_auth->updateUserActivity( 'lock' ),
+			\Notes\AuthStatus::PermProviderError	=> ( function() {
+				$this->_auth->updateUserActivity( 'lock' );
+				$this->controller->run( 
+					'user_locked', [ 
+						'provider'	=> $this, 
+						'user'		=> $user,
+						'status'	=> $status
+					]
+				);
+			} )(),
 			
 			// Reset lookup on check errors
 			\Notes\AuthStatus::RealmError,
@@ -219,8 +327,16 @@ class IDProvider extends Provider {
 			\Notes\AuthStatus::RoleError,
 			\Notes\AuthStatus::SessionError,
 			\Notes\AuthStatus::UserError,
-			\Notes\AuthStatus::CookieError	=> 
-				$this->_auth->resetLookup()
+			\Notes\AuthStatus::CookieError	=> ( function() {
+				$this->_auth->resetLookup();
+				$this->controller->run( 
+					'user_auth_error', [ 
+						'provider'	=> $this, 
+						'user'		=> $user,
+						'status'	=> $status
+					]
+				);
+			} )()
 		};
 	}
 }
