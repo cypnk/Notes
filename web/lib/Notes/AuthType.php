63c168eb9cddd68c271a868f355a407987af8ed5
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-04 12:21:44 -0500

    Update AuthType.php

--- web/lib/Notes/AuthType.php
+++ web/lib/Notes/AuthType.php
@@ -9,5 +9,25 @@ enum AuthType {
 	case External;
 	
 	case Unknown;
+	
+	/**
+	 *  Check authentication scheme from sent header against supported types
+	 */
+	public static function mode( string $auth, string $method ) : static {
+		$lauth = \strtolower( $auth );
+		
+		return
+		match( true ) {
+			\str_starts_with( $lauth, 'basic' )	=> AuthType::Basic,
+			\str_starts_with( $lauth, 'digest' )	=> AuthType::Digest,
+			\str_starts_with( $lauth, 'bearer' )	=> AuthType::External,
+			
+			// Try post
+			( 0 == \strcasecmp( 'post', $method ) )	=> AuthType::Form,
+			
+			// Fallback
+			default					=> AuthType::Unknown
+		};
+	}
 }
 
