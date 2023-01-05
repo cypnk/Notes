9ec034b5bd7469f6e52f83b5aa13841571c425c6
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-04 21:58:02 -0500

    Update Form.php

--- web/lib/Notes/Form.php
+++ web/lib/Notes/Form.php
@@ -17,88 +17,51 @@ abstract class Form {
 	public readonly string $id;
 	
 	/**
+	 *  Submission type
+	 *  @var string
+	 */
+	public readonly string $method;
+	
+	/**
 	 *  Form encoding type, defaults to 'application/x-www-form-urlencoded'
 	 *  @var string
 	 */
 	public readonly string $enctype;
 	
 	/**
-	 *  Basic form templates (uses Tachyons.css)
+	 *  List of inputs
 	 *  @var array
 	 */
-	protected static array $templates	= [
-		// Basic form
-		'form'		=>
-		'<form name="{name}" id="{id}" 
-			class="pa4 black-80 measure" action="{action}" 
-			method="{method}" enctype="{enctype}">' . 
-		'<legend class="mb2 f4 measure">{legend}</legend>{body}</form>',
-		
-		// No legend
-		'form_nl'	=>
-		'<form  name="{name}" id="{id}" 
-			class="pa4 black-80 measure" action="{action}" 
-			method="{method}" enctype="{enctype}">{body}</form>'
-	];
+	public array $inputs;
 	
 	abstract public function render( array $data ) : string {}
 	
 	/**
 	 *  Basic form constructor
 	 *  
+	 *  @param string	$_inputs	List of form inputs
 	 *  @param string	$_name		Form unique label
 	 *  @param string	$_method	Submission method
 	 *  @param string	$_enc		Content encoding type
 	 *  @param string	$_id		Optional, on-page identifier
 	 */
 	public function __construct( 
+		array	$_inputs.
 		string	$_name, 
 		string	$_method,
 		string	$_enc, 
-		?string	$_id = null 
+		?string	$_id		= null 
 	) {
 		$this->name	= $_name;
 		if ( empty( $_id ) ) {
 			$this->id = $_name;
 		}
 		
-		$this->method	= static::cleanFormMethodType( $_method );
-		
-		
-		// Apply only valid enctypes
-		$this->enctype = static::cleanFormEnctype( $_enc );
+		$this->method	= $_method;
+		$this->enctype	= $_enc;
+		$this->inputs	= $_inputs;
 	}
 	
-	/**
-	 *  Form encoding type helper, defaults to 'application/x-www-form-urlencoded'
-	 *  
-	 *  @param string	$v	Raw encoding type
-	 *  @return string
-	 */
-	public static function cleanFormEnctype( string $v ) : string {
-		static $ec = 	=  [
-			'application/x-www-form-urlencoded',
-			'multipart/form-data',
-			'text/plain'
-		];
-		
-		$v = \strtolower( trim( $v ) );
-		return \in_array( $v, $ec ) ?
-			$v : 'application/x-www-form-urlencoded';
-	}
-	
-	/**
-	 *  Filter form sending method type, defaults to get or post
-	 *  
-	 *  @param string	$v	Raw form method
-	 *  @return string
-	 */
-	public static function cleanFormMethodType( string $v ) : string {
-		static $fm = [ 'get', 'post' ];
-		
-		$v = \strtolower( trim( $v ) );
-		return \in_array( $v, $fm ) ? $v : 'get';
-	}
 	
 	/**
 	 *  Initiate field token or reset existing
