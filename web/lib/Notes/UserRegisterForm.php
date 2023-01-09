9b55cb33c8166ccaf99e933df1baad0b0111ab9a
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-08 19:47:13 -0500

    Update UserRegisterForm.php

--- web/lib/Notes/UserRegisterForm.php
+++ web/lib/Notes/UserRegisterForm.php
@@ -4,39 +4,17 @@ namespace Notes;
 
 class UserRegisterForm extends Form {
 	
-	public function __construct( \Notes\Controller $ctrl, string $action = '' ) {
-		parent::__construct( $ctrl );
-		
-		// Load form definition from database
-		$db	= $this->getControllerParam( '\\\Notes\\Data' );
-		
-		$form	= 
-		$db->dataExec( 
-			'SELECT content FROM forms WHERE label = :label' 
-			[ ':label' => 'web user register' ], 
-			'result', 
-			\DATA
-		);
-		
-		if ( empty( $form ) ) {
-			return;
-		}
-		
-		$this->params	= $form[0];
-		
-		// Form type
-		$this->form_type = 
-		( $this->params['form']['validated'] ?? true ) ? 
-			\Notes\FormType::Validated : 
-			\Notes\FormType::UnValidated;
-		
-		// Override action
-		if ( !empty( $action ) ) {
-			$this->_params['form']['action']  = $action;
-		}
-		
-		// TODO: Add meta key and anti-XSRF tokens
-		// TODO: Replace language placeholders with definitions in config
+	public static function newForm(
+		\Notes\Controller	$ctrl,
+		string			$action = ''
+	) {
+		return 
+		static::loadForm( 
+			$ctrl, 
+			'user register form', 
+			'\\Notes\\UserRegisterForm', 
+			$action
+		); 
 	}
 	
 	public function render() : string {
