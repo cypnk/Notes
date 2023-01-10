938f89d1d1d95782247b110cffcfef6ef18f612d
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-10 14:53:58 -0500

    Create ContentForm.php

---
+++ web/lib/Notes/ContentForm.php
@@ -0,0 +1,35 @@
+<?php declare( strict_types = 1 );
+
+namespace Notes;
+
+class ContentForm extends Form {
+	
+	public int $id;
+	
+	public array $placeholders		= [];
+	
+	protected static string $form_name	= 'web_content';
+	
+	public function render() : string {
+		// TODO: Load language defintions
+		if ( !isset( $this->rendered ) ) {
+			$this->rendered = 
+			$this->form_type->render( 
+				$this->controller, $this->params['form'] 
+			)
+		}
+		
+		// Gather event placeholder replacements
+		$this->controller->run( 
+			static::$form_name, [ 'form' => $this ] 
+		);
+		
+		// Apply placeholders to output
+		return 
+		\strtr( $this->rendered, [
+			...$this->placeholders, 
+			...$this->controller->output( static::$form_name ) 
+		] );
+	}
+	
+}
