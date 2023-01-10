42b49496e604ce8cd97217bd3027003db9bfc0d2
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-10 02:44:06 -0500

    Create DoctypeForm.php

---
+++ web/lib/Notes/DoctypeForm.php
@@ -0,0 +1,53 @@
+<?php declare( strict_types = 1 );
+
+namespace Notes;
+
+class DoctypeForm extends Form {
+	
+	public int $id;
+	
+	public string $label;
+	
+	public string $description;
+	
+	
+	public static function newForm(
+		\Notes\Controller	$ctrl,
+		string			$action = ''
+	) {
+		return 
+		static::loadForm( 
+			$ctrl, 
+			'web_doctype', 
+			'\\Notes\\DoctypeForm', 
+			$action
+		); 
+	}
+	
+	public function render() : string {
+		// TODO: Load language defintions
+		$form = 
+		$this->form_type->render( 
+			$this->controller, $this->params['form'] 
+		)
+		
+		// Gather event placeholder replacements
+		$this->controller->run( 
+			'doctype_form', [ 'form' => $this ] 
+		);
+		
+		$params = 
+		\array_merge( 
+			[ 
+				'{id}'		=> ( string ) ( $this->id ?? '' ),
+				'{label}'	=> $this->label ?? '',
+				'{description}'	=> $this->description ?? ''
+			],
+			$this->controller->output( 'doctype_form' )
+		];
+		
+		// Apply placeholders to output
+		return \strtr( $form, $params );
+	}
+}
+
