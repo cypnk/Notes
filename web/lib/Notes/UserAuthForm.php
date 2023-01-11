291f2bd7a68eec3b80769a9a592b02484fc74080
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-11 05:28:24 -0500

    Update UserAuthForm.php

--- web/lib/Notes/UserAuthForm.php
+++ web/lib/Notes/UserAuthForm.php
@@ -17,36 +17,10 @@ abstract class UserAuthForm extends Form {
 	protected string $form_name	= 'web_user_auth';
 
 	public function render() : string {
-		// Generate HTML output
-		if ( !isset( $this->rendered ) ) {
-			$this->rendered = 
-			$this->form_type->render( 
-				$this->controller, $this->params['form'] 
-			)
-		}
-		
-		// Generate event placeholder content by current form name
-		$this->controller->run( 
-			static::$form_name, [ 'form' => $this ] 
-		);
-		
-		// Generate anti-XSRF tokens
-		$pair	= 
-		$this->genNoncePair( 
-			$this->getControllerParam( '\\Notes\\Config' ),
-			static::$form_name
-		);
-		
-		// Return with placeholders replaced
-		// TODO: Load language definitions from databse
-		return 
-		\strtr( $this->rendered, [
-			...new ArrayIterator( [ 
-				'{username}'	=> $this->username ?? '',
-				'{token}'	=> $pair['token'],
-				'{nonce}'	=> $pair['nonce']
-			] ), 
-			...$this->controller->output( static::$form_name )
-		] );
+		$this->placeholders = 
+		[ 
+			'{username}'	=> $this->username ?? ''
+		];
+		return parent::render();	
 	}
 }
