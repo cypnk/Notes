b33e105848cd6a6c4d0cfe8383acfec24c4af68a
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-11 05:27:32 -0500

    Update ContentForm.php

--- web/lib/Notes/ContentForm.php
+++ web/lib/Notes/ContentForm.php
@@ -9,25 +9,10 @@ class ContentForm extends Form {
 	protected static string $form_name	= 'web_content';
 	
 	public function render() : string {
-		// TODO: Load language defintions
-		if ( !isset( $this->rendered ) ) {
-			$this->rendered = 
-			$this->form_type->render( 
-				$this->controller, $this->params['form'] 
-			)
-		}
-		
-		// Gather event placeholder replacements
-		$this->controller->run( 
-			static::$form_name, [ 'form' => $this ] 
-		);
-		
-		// Apply placeholders to output
-		return 
-		\strtr( $this->rendered, [
-			...$this->placeholders, 
-			...$this->controller->output( static::$form_name ) 
-		] );
+		$this->placeholders = [
+			'{id}'		=> ( string ) ( $this->id ?? '' )
+		];
+		return parent::render();
 	}
 	
 }
