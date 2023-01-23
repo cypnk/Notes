7b922de81cc845f9c1d51b9a70828dc58c2096e6
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-23 01:37:10 -0500

    Update HtmlNote.php

--- web/lib/Notes/HtmlNote.php
+++ web/lib/Notes/HtmlNote.php
@@ -36,6 +36,93 @@ class HtmlNote extends Controllable {
 	}
 	
 	/**
+	 *  Create node element with current document
+	 *  
+	 *  @param string	$name		Node element name
+	 *  @param string	$content	Optinoal inner content
+	 *  @param string	$ns		Optional XML namespace
+	 *  @return mixed
+	 */
+	public function createNode( 
+		string		$name,
+		?string		$content	= null,
+		?string		$ns		= null 
+	) : ?\DOMElement {
+		
+		$content	??= '';
+		$ns		= \Notes\Util::cleanUrl( $ns ?? '' );
+		
+		$node		= 
+		match( true ) {
+			!empty( $ns ) && !empty( $content )	=>
+			$this->dom->createElementNS( $ns, $name, $content ),
+			
+			!empty( $ns ) && empty( $content )	=>
+			$this->dom->createElementNS( $ns, $name ),
+			
+			empty( $ns ) && !empty( $content )	=>
+			$this->dom->createElement( $name, $content ),
+			
+			default					=>
+			$this->dom->createElement( $name )
+		};
+		
+		if ( false === $node ) {
+			return null;
+		}
+		
+		return $node;
+	}
+	
+	/**
+	 *  Create text chraracter content with current document
+	 *  
+	 *  @param string	$content	Base element content
+	 *  @param string	$mode		Filtering mode
+	 *  @return mixed
+	 */
+	public function createText( 
+		string	$content,
+		string	$mode		= 'text'
+	) {
+		$node = 
+		match( \strtolower( $mode ) ) {
+			// TODO: Filter basic HTML
+			'html'		=>
+			$this->dom->createCDATASection( $content ),
+			
+			// URL Slug and similar
+			'slug'		=>
+			$this->dom->createTextNode( 
+				\Notes\Util::slugify( $content )
+			),
+			
+			// Plain text without special characters
+			'plain'		=>
+			$this->dom->createTextNode( 
+				\Notes\Util::bland( $content, true )
+			),
+			
+			// Template placeholder
+			'place', 'placeholder'	=>
+			$this->dom->createTextNode(
+				\preg_replace( 
+					'/[^\p{L}\p{N}\:\{\}_]+/', '', 
+					\Notes\Util::unifySpaces( $content, '_' )
+				)
+			),
+			
+			// Everything else
+			default		=>
+			$this->dom->createTextNode( 
+				\Notes\Util::bland( $content )
+			);
+		};
+		
+		return ( false === $node ) ? null : $node;
+	}
+	
+	/**
 	 *  Extract note body
 	 *  
 	 *  @return string
