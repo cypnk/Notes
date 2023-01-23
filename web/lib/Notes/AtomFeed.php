3fdc317f9fbc6c79e8d23dc43c783fbdcec029c6
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-23 02:18:41 -0500

    Create AtomFeed.php

---
+++ web/lib/Notes/AtomFeed.php
@@ -0,0 +1,29 @@
+<?php declare( strict_types = 1 );
+/**
+ *  @file	/lib/web/Notes/AtomFeed.php
+ *  @brief	Available application feature discovery document
+ *  @link	https://www.ietf.org/rfc/rfc4287.txt
+ */
+
+namespace Notes;
+
+class AtomFeed extends AtomBase {
+	
+	/**
+	 *  Main sydnication feed
+	 *  @var \DOMElement
+	 */
+	protected readonly \DOMElement $feed;
+	
+	public function __construct( \Notes\Controller $ctrl ) {
+		parent::__construct( $ctrl );
+		
+		$this->feed = 
+		$this->addNode( 'feed', '', 'atom', true );
+	}
+	
+	/**
+	 *  TODO: Add entries, etc...
+	 */
+}
+
