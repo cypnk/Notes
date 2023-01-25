b4697006ac7c52bb051d2345010edbf7f8e90b80
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2023-01-25 01:10:05 -0500

    Update Entity.php

--- web/lib/Notes/Entity.php
+++ web/lib/Notes/Entity.php
@@ -16,28 +16,42 @@ abstract class Entity implements Stringable extends Controllable {
 	
 	public int $sort_order;
 	
-	public string $setting_id;
+	protected string $_setting_id;
 	
 	protected array $_settings		= [];
 	
-	abstract public function save() : bool {}
+	protected array $_readonly		= [];
+	
+	abstract public function save() : bool;
 	
 	public function __set( $name, $value ) {
 		switch ( $name ) {
 			case 'id':
+				if ( isset( $this->_id ) ) {
+					return;
+				}
 				$this->_id = ( int ) $value;
 				break;
 				
 			case 'uuid':
-				$this->_uuid = $value;
+				if ( isset( $this->_uuid ) ) {
+					return;
+				}
+				$this->_uuid = ( string ) $value;
 				break;
 				
 			case 'created':
-				$this->_created = $value;
+				if ( isset( $this->_created ) ) {
+					return;
+				}
+				$this->_created = ( string ) $value;
 				break;
 				
 			case 'updated':
-				$this->_updated = $value;
+				if ( isset( $this->_updated ) ) {
+					return;
+				}
+				$this->_updated = ( string ) $value;
 				break;
 				
 			case 'settings':
@@ -113,10 +127,38 @@ abstract class Entity implements Stringable extends Controllable {
 				
 			case 'updated':
 				return isset( $this->_updated );
+				
+			case 'setting_id':
+				return 
+					isset( $this->_setting_id ) || 
+					isset( $this->_settings['setting_id'] );
 		}
 		return false;
 	}
 	
+	/**
+	 *  Directly apply settings value externally, if it's not write-protected
+	 *  
+	 *  @param string	$name		Setting key
+	 *  @param mixed	$value		Non-object setting value
+	 *  @return bool
+	 */
+	public function setSetting( string $name, $value ) : bool {
+		if ( \in_array( $name, $this->_readonly ) ) {
+			return false;
+		}
+		
+		$this->_settings[$name] = $value;
+		return true;
+	}
+	
+	/**
+	 *  Find setting value by key name, if it has been set
+	 *  
+	 *  @param string	$name		Setting key to search
+	 *  @param mixed	$default	Fallback value
+	 *  @return mixed
+	 */
 	public function setting( string $name, $default ) {
 		return $this->_settings[$name] ?? $default;
 	}
